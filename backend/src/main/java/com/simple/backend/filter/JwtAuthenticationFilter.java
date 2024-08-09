ef6623f398a77a03e4f7326689a315180edc6d1b
jeongseyoung <sengangel@naver.com>
2024-08-10 00:19:27 +0900

    security filter

--- backend/src/main/java/com/simple/backend/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/simple/backend/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,12 @@ package com.simple.backend.filter;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,6 +20,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+// @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
 
@@ -24,8 +31,31 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        try {
+            String token = parseBearerToken(request);
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = jwtProvider.validtae(token);
+            if (email == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null,
+                    AuthorityUtils.NO_AUTHORITIES);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response);
     }
 
     private String parseBearerToken(HttpServletRequest request) {
@@ -36,6 +66,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return null;
 
         return authorization.substring(7);
-
     }
 }
