41b0c5276906827e8f1b637f00b7149fd60c5cfd
jeongseyoung <sengangel@naver.com>
2026-05-17 23:57:27 +0900

    Update FileController.java

--- backend/src/main/java/com/simple/backend/controller/FileController.java
+++ backend/src/main/java/com/simple/backend/controller/FileController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/file")
+@RequestMapping("/file")   
 @RequiredArgsConstructor
 public class FileController {
 
