4ce73eb878c7e599c2af1ee7e840fc36c3fa0be5
jeongseyoung <sengangel@naver.com>
2026-05-17 23:57:47 +0900

    Update AuthController.java

--- backend/src/main/java/com/simple/backend/controller/AuthController.java
+++ backend/src/main/java/com/simple/backend/controller/AuthController.java
@@ -11,7 +11,7 @@ import com.simple.backend.service.AuthService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
+     
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
