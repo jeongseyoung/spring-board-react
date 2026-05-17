4a3396c65851286ea3417e612076743cc6950f83
jeongseyoung <sengangel@naver.com>
2026-05-17 23:57:53 +0900

    Update BoardListViewRepository.java

--- backend/src/main/java/com/simple/backend/repository/BoardListViewRepository.java
+++ backend/src/main/java/com/simple/backend/repository/BoardListViewRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.simple.backend.entity.BoardListViewEntity;
-
+      
 @Repository
 public interface BoardListViewRepository extends JpaRepository<BoardListViewEntity, Integer> {
     List<BoardListViewEntity> findByOrderByWriteDatetimeDesc();
